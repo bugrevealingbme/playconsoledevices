d7d4e903e2c8460f63128d8acf7d979b53c4e626
Burak Mete Erdoğan <bugrevealingbme@gmail.com>
2024-10-12 13:44:35 +0300

    Update PlayConsoleDeviceExport.js

--- PlayConsoleDeviceExport.js
+++ PlayConsoleDeviceExport.js
@@ -13,17 +13,24 @@ if (table) {
         const rowData = {};
 
         // Hücreleri sırayla alın
-        const deviceCell = row.querySelector('ess-cell[essfield="model"] .line');
+        const deviceCell = row.querySelector('ess-cell[essfield="device"] .line');
+        const modellCell = row.querySelector('ess-cell[essfield="model"] .line');
         const marketNameCell = row.querySelector('ess-cell[essfield="marketing"] text-field');
         const ramCell = row.querySelector('ess-cell[essfield="ram"] text-field');
+        const socCell = row.querySelector('ess-cell[essfield="soc"] text-field');
         const androidVersionsCell = row.querySelector('ess-cell[essfield="sdk"] text-field');
+        const launchDateCell = row.querySelector('ess-cell[essfield="launch_date"] text-field');
         const imageCell = row.querySelector('.image-container img');
+        
 
         // Hücrelerin içeriğini kontrol ederek doldurun
         if (deviceCell) rowData.device = deviceCell.textContent.trim();
+        if (modellCell) rowData.model = modellCell.textContent.trim();
         if (marketNameCell) rowData.marketName = marketNameCell.textContent.trim();
         if (ramCell) rowData.ram = ramCell.textContent.trim();
+        if (socCell) rowData.soc = socCell.textContent.trim();
         if (androidVersionsCell) rowData.androidVersions = androidVersionsCell.textContent.trim();
+        if (launchDateCell) rowData.launchDate = launchDateCell.textContent.trim();
         if (imageCell) rowData.imageSrc = imageCell.getAttribute('src');
 
         // Her bir satırı data dizisine ekleyin
